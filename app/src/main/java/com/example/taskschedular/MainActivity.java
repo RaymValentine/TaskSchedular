6b8113ddff9939be6402d0a72f0d4011ac888d49
owner <raym_valentine@yahoo.co.jp>
2015-02-22 22:43:52 +0900

    ・選択日付の効果修正 ・Butter Knife導入

--- app/src/main/java/com/example/taskschedular/MainActivity.java
+++ app/src/main/java/com/example/taskschedular/MainActivity.java
@@ -4,19 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends ActionBarActivity  {
+
+    private Integer intTxtSizeNormalView = 12;   // テキストサイズセット（通常サイズ）
+    private Integer intTxtSizeSelectlView = 18;  // テキストサイズセット（選択サイズ）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
         //初期設定
         Calendar cal = Calendar.getInstance();
@@ -26,35 +31,66 @@ public class MainActivity extends ActionBarActivity  {
         showYear        = cal.get(Calendar.YEAR);//現在の年を取得
         showMonth       = cal.get(Calendar.MONTH) + 1;//現在の月を取得
         createCalendar();
+    }
 
-        ((Button)findViewById(R.id.btnNextMonth)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (showMonth == 12)
-                {
-                    showYear  = showYear + 1;
-                    showMonth = 1;
-                }
-                else
-                {
-                    showMonth = showMonth + 1;
-                }
-                createCalendar();
-            }
-        });
-        ((Button)findViewById(R.id.btnPreviousMonth)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (showMonth == 1) {
-                    showYear = showYear - 1;
-                    showMonth = 12;
+    @OnClick(R.id.btnNextMonth)
+    void clickNextMonth() {
+        if (showMonth == 12)
+        {
+            showYear  = showYear + 1;
+            showMonth = 1;
+        }
+        else
+        {
+            showMonth = showMonth + 1;
+        }
+        createCalendar();
+    }
+
+    @OnClick(R.id.btnPreviousMonth)
+    void btnPreviousMonth() {
+        if (showMonth == 1) {
+            showYear = showYear - 1;
+            showMonth = 12;
+        } else {
+            showMonth = showMonth - 1;
+        }
+        createCalendar();
+    }
+
+    @OnClick({ R.id.txtDay00, R.id.txtDay01, R.id.txtDay02, R.id.txtDay03, R.id.txtDay04, R.id.txtDay05, R.id.txtDay06
+            , R.id.txtDay10, R.id.txtDay11, R.id.txtDay12, R.id.txtDay13, R.id.txtDay14, R.id.txtDay15, R.id.txtDay16
+            , R.id.txtDay20, R.id.txtDay21, R.id.txtDay22, R.id.txtDay23, R.id.txtDay24, R.id.txtDay25, R.id.txtDay26
+            , R.id.txtDay30, R.id.txtDay31, R.id.txtDay32, R.id.txtDay33, R.id.txtDay34, R.id.txtDay35, R.id.txtDay36
+            , R.id.txtDay40, R.id.txtDay41, R.id.txtDay42, R.id.txtDay43, R.id.txtDay44, R.id.txtDay45, R.id.txtDay46
+            , R.id.txtDay50, R.id.txtDay51, R.id.txtDay52, R.id.txtDay53, R.id.txtDay54, R.id.txtDay55, R.id.txtDay56
+    })
+    void clickTxtDay(TextView textView) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                name = "txtDay" + String.valueOf(i) + String.valueOf(j);
+                resId = getResources().getIdentifier(name, "id", getPackageName());
+                TextView textView1 = (TextView) findViewById(resId);
+
+                textView1.setTextSize(intTxtSizeNormalView);   // テキストサイズセット（通常サイズ）
+
+                //背景セット//0:前月　1:今月　2:次月
+                if (monthflg[i][j] == 1) {
+                    textView1.setBackgroundResource(R.drawable.background_nowmonth);
                 } else {
-                    showMonth = showMonth - 1;
+                    textView1.setBackgroundResource(R.drawable.background_prenex);
                 }
-                createCalendar();
+                // 今日の背景をセット
+                if (calendarMatrix[i][j] == nowDay && showYear == nowYear && showMonth == nowMonth) {
+                    textView1.requestFocus();
+                    textView1.setBackgroundResource(R.drawable.background_today);
+                }
             }
-        });
+        }
 
+        textView.setTextSize(intTxtSizeSelectlView);   // テキストサイズセット（選択サイズ）
+        textView.requestFocus();
+        textView.setBackgroundResource(R.drawable.background_selectday);
     }
 
     @Override
@@ -99,10 +135,10 @@ public class MainActivity extends ActionBarActivity  {
     private int resId;          //
     private int len;            //
     private int[][] calendarMatrix = new int[6][7];   //カレンダー情報テーブル
+    private int[][] monthflg = new int[6][7];        //0:前月　1:今月　2:次月
 
     //カレンダー作成
     private void createCalendar() {
-        int[][] monthflg = new int[6][7];        //0:前月　1:今月　2:次月
 
         Calendar calendar = Calendar.getInstance();
         calendar.clear();
@@ -175,7 +211,7 @@ public class MainActivity extends ActionBarActivity  {
                 name = "txtDay" + String.valueOf(i) + String.valueOf(j);
                 resId = getResources().getIdentifier(name, "id", getPackageName());
                 TextView textView1 = (TextView) findViewById(resId);
-                //
+
                 len = Integer.toString(calendarMatrix[i][j]).length();
                 if (len == 1) {
                     textView1.setText(String.valueOf(String.format("%1$2d", calendarMatrix[i][j])) + "\n" + "\n");
@@ -198,6 +234,8 @@ public class MainActivity extends ActionBarActivity  {
                     textView1.requestFocus();
                     textView1.setBackgroundResource(R.drawable.background_today);
                 }
+
+                textView1.setTextSize(intTxtSizeNormalView);   // テキストサイズセット（通常サイズ）
             }
         }
     }
