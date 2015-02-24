33de68c1d5265731d0a4e6eee3b81db97729778e
owner <raym_valentine@yahoo.co.jp>
2015-02-24 22:22:02 +0900

    前月も色が変わるように改めて修正

--- app/src/main/java/com/example/taskschedular/MainActivity.java
+++ app/src/main/java/com/example/taskschedular/MainActivity.java
@@ -14,8 +14,28 @@ import butterknife.OnClick;
 
 public class MainActivity extends ActionBarActivity  {
 
-    private Integer intTxtSizeNormalView = 12;   // テキストサイズセット（通常サイズ）
-    private Integer intTxtSizeSelectlView = 18;  // テキストサイズセット（選択サイズ）
+    //変数宣言
+    private int showYear;       //対象の西暦年
+    private int showMonth;      //対象の月
+    private int nowYear;
+    private int nowMonth;
+    private int nowDay;
+    private int dayCount;       //
+    private int startDay;       //先頭曜日
+    private int lastDate1;      //月末日付1
+    private int lastDate2;      //月末日付2
+    private boolean isStart;    //
+    private boolean isEnd;      //
+    private int x;              //
+    private int y;              //
+    private String name;        //
+    private int resId;          //
+    private int len;            //
+    private int[][] calendarMatrix = new int[6][7]; // カレンダー情報テーブル
+    private int[][] monthFlg = new int[6][7];       // 0:前月　1:今月　2:次月
+
+    private Integer intTxtSizeNormalView = 12;      // テキストサイズセット（通常サイズ）
+    private Integer intTxtSizeSelectlView = 18;     // テキストサイズセット（選択サイズ）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +45,11 @@ public class MainActivity extends ActionBarActivity  {
 
         //初期設定
         Calendar cal = Calendar.getInstance();
-        nowYear         = cal.get(Calendar.YEAR);//現在の年を取得
-        nowMonth        = cal.get(Calendar.MONTH) + 1;//現在の月を取得
-        nowDay          = cal.get(Calendar.DATE);//現在の日を取得
-        showYear        = cal.get(Calendar.YEAR);//現在の年を取得
-        showMonth       = cal.get(Calendar.MONTH) + 1;//現在の月を取得
+        nowYear         = cal.get(Calendar.YEAR);       //現在の年を取得
+        nowMonth        = cal.get(Calendar.MONTH) + 1;  //現在の月を取得
+        nowDay          = cal.get(Calendar.DATE);       //現在の日を取得
+        showYear        = cal.get(Calendar.YEAR);       //現在の年を取得
+        showMonth       = cal.get(Calendar.MONTH) + 1;  //現在の月を取得
         createCalendar();
     }
 
@@ -97,29 +117,9 @@ public class MainActivity extends ActionBarActivity  {
     }
 
 
-
-    //変数宣言
-    public int showYear;         //対象の西暦年
-    public int showMonth;       //対象の月
-    public int nowYear;
-    public int nowMonth;
-    public int nowDay;
-    private int dayCount;       //
-    private int startDay;       //先頭曜日
-    private int lastDate1;      //月末日付1
-    private int lastDate2;      //月末日付2
-    private boolean isStart;    //
-    private boolean isEnd;      //
-    private int x;              //
-    private int y;              //
-    private String name;        //
-    private int resId;          //
-    private int len;            //
-    private int[][] calendarMatrix = new int[6][7];   //カレンダー情報テーブル
-    private int[][] monthflg = new int[6][7];        //0:前月　1:今月　2:次月
-
-    //カレンダー作成
+    // カレンダー作成
     private void createCalendar() {
+        monthFlg = new int[6][7];                       // ここでは初期化を行う
 
         Calendar calendar = Calendar.getInstance();
         calendar.clear();
@@ -127,60 +127,60 @@ public class MainActivity extends ActionBarActivity  {
         TextView textView = (TextView)this.findViewById(R.id.txtYM);
         textView.setText(showYear + "年" + showMonth + "月");
         // 月の初めの曜日を求めます。
-        calendar.set(showYear, showMonth - 1, 1); // 引数: 1月: 0, 2月: 1, ...
-        startDay = calendar.get(Calendar.DAY_OF_WEEK);//曜日を取得
+        calendar.set(showYear, showMonth - 1, 1);       // 引数: 1月: 0, 2月: 1, ...
+        startDay = calendar.get(Calendar.DAY_OF_WEEK);  // 曜日を取得
         // 今月末の日付を求めます。
         calendar.add(Calendar.MONTH, 1);
         calendar.add(Calendar.DATE, -1);
-        lastDate1 = calendar.get(Calendar.DATE);//日を取得
+        lastDate1 = calendar.get(Calendar.DATE);        // 日を取得
         dayCount = 1;
         // 前月末の日付を求めます。
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         calendar.add(Calendar.DATE, -1);
-        lastDate2 = calendar.get(Calendar.DATE);//日を取得
-        //初期値セット
+        lastDate2 = calendar.get(Calendar.DATE);        // 日を取得
+        // 初期値セット
         isStart = false;
         isEnd = false;
-        //前月末の日付用
+        // 前月末の日付用
         x = 0;
         y = 0;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
-                //先頭曜日確認
+                // 先頭曜日確認
                 // startDay: 日曜日 = 1, 月曜日 = 2, ...
                 if (!isStart && (startDay - 1) == j) {
-                    //日にちセット開始
+                    // 日にちセット開始
                     isStart = true;
-                    //今月のカレンダーに前月末表示用
+                    // 今月のカレンダーに前月末表示用
                     y = i;
                     x = j;
                     lastDate2 = lastDate2 - (startDay - 2);
                 }
                 if (isStart) {
-                    //終了日まで行ったか
+                    // 終了日まで行ったか
                     calendarMatrix[i][j] = dayCount;
                     if (!isEnd)
                     {
-                        monthflg[i][j] = 1;
+                        monthFlg[i][j] = 1;
                     }
                     else
                     {
-                        monthflg[i][j] = 2;
+                        monthFlg[i][j] = 2;
                     }
-                    //カウント＋１
+                    // カウント＋１
                     dayCount++;
-                    //終了確認
+                    // 終了確認
                     if(dayCount > lastDate1)
                     {
                         isEnd = true;
-                        //来月初をセット
+                        // 来月初をセット
                         dayCount = 1;
                     }
                 }
             }
         }
 
-        //前月末を改めて挿入
+        // 前月末を改めて挿入
         for (int i = 0; i <= y; i++) {
             for (int j = 0; j < x; j++) {
                 calendarMatrix[i][j] = lastDate2;
@@ -206,8 +206,8 @@ public class MainActivity extends ActionBarActivity  {
                     textView1.setText(String.valueOf(calendarMatrix[i][j]) + "\n" + "\n");
                 }
 
-                //背景セット//0:前月　1:今月　2:次月
-                if (monthflg[i][j] == 1)
+                // 背景セット// 0:前月　1:今月　2:次月
+                if (monthFlg[i][j] == 1)
                 {
                     textView1.setBackgroundResource(R.drawable.background_nowmonth);
                 }
